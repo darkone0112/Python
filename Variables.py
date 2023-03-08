cbd1ea127f0c810daea4c2b2f5cdafd9157fce88
darkone0112 <darkone0112@gmail.com>
2023-03-08 16:31:01 +0100

    someMoreCode

---
+++ Variables.py
@@ -0,0 +1,10 @@
+name = "World"
+print("Hello" + name)
+full_hello = "Hello" + name
+print("The dataType of name is" + type(name))
+print(full_hello)
+
+age = 21
+age += 1
+print(age)
+print("The dataType of age is" + type(age))
