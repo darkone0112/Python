2426be85f737ca80cd4e2f83136a0f1fb9f980bc
darkone0112 <darkone0112@gmail.com>
2023-04-18 15:52:23 +0200

    Functions

--- learning_py/Functions.py
+++ learning_py/Functions.py
@@ -1,7 +1,7 @@
 # function = block of code that is executed when it is called
 
 def hello(name):
-    print("Hello" + name)
+        print ("Hello " + name)
 
 name = input("Enter your name: ")
 hello(name)
